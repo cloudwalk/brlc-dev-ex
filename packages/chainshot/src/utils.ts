92ac5f5173b4e0cca090982812e3fe8778d5f1c9
Ilia Groshev <ilia.groshev@cloudwalk.io>
2025-09-15 15:31:08 +0100

    prettify some output

--- packages/chainshot/src/utils.ts
+++ packages/chainshot/src/utils.ts
@@ -1,5 +1,12 @@
 import { Addressable } from "ethers";
 
+
+function limitStringLength(str: string, limit: number): string {
+  if (str.length <= limit) return str;
+  const halfLimit = Math.floor(limit / 2);
+  return str.slice(0, halfLimit) + ".." + str.slice(-halfLimit);
+}
+
 export function revertMap<K extends string, R>(map: Record<K, R>, mapper: (value: R) => string): Record<string, K> {
   return Object.fromEntries(Object.entries(map).map(([key, value]) => [mapper(value as R), key])) as Record<string, K>;
 }
@@ -29,16 +36,29 @@ export function mapRecordValues<T, R, K extends string>(
   ) as Record<K, R>;
 }
 
+const knownConstants = {
+  "0x0000000000000000000000000000000000000000": "ZERO_ADDR",
+  "0x0000000000000000000000000000000000000000000000000000000000000000": "ZERO",
+  "0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff": "MAX_UINT256",
+} as const;
+
 export function stringifyValue(value: unknown): string {
-  if (typeof value === "bigint") return value.toString();
-  if (typeof value === "string") return value;
+  if (typeof value === "string") {
+    const resolvedKnownConstant = knownConstants[value.toLowerCase() as keyof typeof knownConstants];
+    return resolvedKnownConstant ?? limitStringLength(value, 10);
+  };
+
+  if (typeof value === "bigint")  {
+    return stringifyValue(value.toString());
+  };
+
   if (Array.isArray(value)) return `[${value.map(stringifyValue).join(", ")}]`;
   if (typeof value === "object" && value !== null) {
     try {
-      return JSON.stringify(value, (_, v) => typeof v === "bigint" ? v.toString() : v);
+      return JSON.stringify(value, (_, v) => stringifyValue(v));
     } catch {
-      return String(value);
+      return stringifyValue(String(value));
     }
   }
-  return String(value);
+  return stringifyValue(String(value));
 }
